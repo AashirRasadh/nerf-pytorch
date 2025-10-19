5830b22bd7d94f7f805d331a3003ec4442d3e9fe
Aashir Rasadh <159936314+AashirRasadh@users.noreply.github.com>
2025-10-19 16:54:44 +0530

    Update run_nerf.py

--- run_nerf.py
+++ run_nerf.py
@@ -519,13 +519,13 @@ def config_parser():
     # logging/saving options
     parser.add_argument("--i_print",   type=int, default=100, 
                         help='frequency of console printout and metric loggin')
-    parser.add_argument("--i_img",     type=int, default=500, 
+    parser.add_argument("--i_img",     type=int, default=1000, 
                         help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=10000, 
+    parser.add_argument("--i_weights", type=int, default=2000, 
                         help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=50000, 
+    parser.add_argument("--i_testset", type=int, default=2500, 
                         help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=50000, 
+    parser.add_argument("--i_video",   type=int, default=2500, 
                         help='frequency of render_poses video saving')
 
     return parser
